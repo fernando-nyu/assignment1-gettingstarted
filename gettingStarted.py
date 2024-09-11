6aa0cd1cdad0de91a21b3fa65c866b1a2c149aa6
Fernando Gonzalez <fg2482@nyu.edu>
2024-09-11 00:15:06 -0400

    Fix answer to DNS and ICMP question

--- gettingStarted.py
+++ gettingStarted.py
@@ -11,8 +11,8 @@ def welcome_assignment_answers(question):
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
         "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": "7c553fe0663bfe748cf99685d577034519a6913e215abcd57cc2bf6928f9c485",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 4,
-        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 2
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 5,
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3
     }
     return cases.get(question, "Question not found.")
 # Complete all the questions.
